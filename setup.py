ce9e93f7382f83b83c5f08a7f4742a9ed9b20957
daohu527 <daohu527@gmail.com>
2022-04-15 20:39:14 +0800

    add pip support

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cyber_record",
+    version="0.0.1",
+    author="daohu527",
+    author_email="daohu527@gmail.com",
+    description="Cyber record offline parse tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daohu527/cyber_record",
+    project_urls={
+        "Bug Tracker": "https://github.com/daohu527/cyber_record/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "cyber_record"},
+    packages=setuptools.find_packages(where="cyber_record"),
+    python_requires=">=3.6",
+)
