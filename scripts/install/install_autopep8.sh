5d6d4a3a28e75a9b44b30d2914cef7bfbc25933a
daohu527 <daohu527@gmail.com>
2024-08-13 17:42:41 +0800

    feat: add lint

--- scripts/install/install_autopep8.sh
+++ scripts/install/install_autopep8.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-sudo apt-get update
-
-# Install pip (if not installed)
-echo "Checking if pip is installed..."
-if ! command -v pip &> /dev/null
-then
-    echo "pip is not installed. Installing pip..."
-    sudo apt-get install -y python3-pip
-fi
-
 # Install autopep8
 echo "Installing autopep8..."
 pip3 install --user autopep8
